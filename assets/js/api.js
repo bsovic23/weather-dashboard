0af96812f01bbec5f7852c6af90f296bd4082816
Brit Sovic <bsovic@tulane.edu>
2022-11-07 16:57:07 -0500

    creating input form

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,33 @@
-console.log('this js works');
+// API URL
 
-//This javascript file works
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// Form Box Variables
+var cityForm = document.querySelector("#cityForm");
+var cityEl = document.querySelector("#cityname");
+
+// Results Box Variables
+var cityWeatherEl = document.querySelector("#cityWeather");
+
+// Search City Function
+var getCity = function(event) {
+    event.preventDefault();
+
+    var cityName = cityEl.value.trim();
+    console.log(cityName);
+
+    cityWeather(cityName);
+
+    cityEl.value = "";
+};
+
+// Display City Results
+var cityWeather = function(city) {
+    
+    var liEl = document.createAttribute('li');
+    liEl.textContent = city;
+    
+    cityWeatherEl.appendChild(liEl);
+}
+
+cityForm.addEventListener('submit', getCity);
