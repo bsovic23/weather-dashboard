03147e3a61c72d3b378c3b61d97b8c675f1d6223
Brit Sovic <bsovic@tulane.edu>
2022-11-09 12:48:58 -0500

    updated the search bar arry for previous searches

--- assets/js/api.js
+++ assets/js/api.js
@@ -9,12 +9,14 @@ var cityEl = document.querySelector("#cityname");
 // Results Box Variables
 var cityWeatherEl = document.querySelector("#cityWeather");
 
+// City Search History
+citySearchHistory = [];
+
 // Search City Function
 var getCity = function(event) {
     event.preventDefault();
 
     var cityName = cityEl.value.trim();
-    console.log(cityName);
 
     cityWeather(cityName);     
 
@@ -23,16 +25,18 @@ var getCity = function(event) {
 
 // Display City Results
 var cityWeather = function(city) {
-    
-    console.log(city);
 
-    // ABOVE ALL WORKS, below does not work
-    
+    if (!citySearchHistory.length === 0) {
+        citySearchHistory.push(city);
+    } else {
+        citySearchHistory.push(city);
+    }
+
+    console.log(citySearchHistory);
     /*
-    var liEl = document.createAttribute('div');
-    liEl.textContent = cityEl;
-    
-    cityWeatherEl.appendChild(liEl);
+    for (i=0; i < citySearchHistory.length; i++) {
+        // insert appending to the list below search history here
+    }
     */
 }
 
